faadc32db126f2926f482aed8b1c9d8210094642
diego41071 <alexander.diego.2007@hotmail.com>
2019-08-14 17:25:02 -0500

    cambios menores

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 
-    $(window).scroll(function() {
-        $('#animatedElement').each(function(){
-        var imagePos = $(this).offset().top;
+$(window).scroll(function () {
+  $('#animatedElement').each(function () {
+    var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+400) {
-                $(this).addClass("slideUp");
-            }
-        });
-    });
-
-    var slideIndex = 1;
-        showDivs(slideIndex);
-
-        function plusDivs(n) {
-          showDivs(slideIndex += n);
-        }
-
-        function showDivs(n) {
-          var i;
-          var x = document.getElementsByClassName("mySlides");
-          if (n > x.length) {slideIndex = 1}
-          if (n < 1) {slideIndex = x.length}
-          for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
-          }
-          x[slideIndex-1].style.display = "block";  
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 400) {
+      $(this).addClass("slideUp");
     }
+  });
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
